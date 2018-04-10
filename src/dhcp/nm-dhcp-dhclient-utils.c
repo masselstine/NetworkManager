a5559242121ad92f741b817c13fcb561fc035ca4
Francesco Giudici <fgiudici@redhat.com>
2018-04-10 11:40:29 +0200

    dhclient: overwrite DUID in lease file when a new duid is specified
    We have just introduced the brand new dhcp-duid property to allow to
    take control of the DUID in DHCPv6: use the specified value when
    provided.
    Note that the DUID in the lease file is still the default option when no
    duid value is provided or when the "lease" option is explicitly stated.

--- src/dhcp/nm-dhcp-dhclient-utils.c
+++ src/dhcp/nm-dhcp-dhclient-utils.c
@@ -611,19 +611,9 @@ nm_dhcp_dhclient_save_duid (const char *leasefile,
 			return FALSE;
 		}
 
-		/* If the file already contains an uncommented DUID, leave it */
 		g_assert (contents);
 		lines = g_strsplit_set (contents, "\n\r", -1);
 		g_free (contents);
-		for (iter = lines; iter && *iter; iter++) {
-			l = *iter;
-			while (g_ascii_isspace (*l))
-				l++;
-			if (g_str_has_prefix (l, DUID_PREFIX)) {
-				g_strfreev (lines);
-				return TRUE;
-			}
-		}
 	}
 
 	s = g_string_sized_new (len + 50);
@@ -632,6 +622,13 @@ nm_dhcp_dhclient_save_duid (const char *leasefile,
 	/* Preserve existing leasefile contents */
 	if (lines) {
 		for (iter = lines; iter && *iter; iter++) {
+			/* If the file contains an uncommented DUID, skip it */
+			l = *iter;
+			while (g_ascii_isspace (*l))
+				l++;
+			if (g_str_has_prefix (l, DUID_PREFIX))
+				continue;
+
 			if (*iter[0])
 				g_string_append (s, *iter);
 			/* avoid to add an extra '\n' at the end of file */
